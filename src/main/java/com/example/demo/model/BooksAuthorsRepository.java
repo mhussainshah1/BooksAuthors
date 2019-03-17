ce92bec6991c66cb1bea6c1d9a5df373bb07fd81
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-17 18:17:14 -0400

    initial commit

---
+++ src/main/java/com/example/demo/model/BooksAuthorsRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BooksAuthorsRepository extends CrudRepository<BooksAuthor, Long> {
+
+}
