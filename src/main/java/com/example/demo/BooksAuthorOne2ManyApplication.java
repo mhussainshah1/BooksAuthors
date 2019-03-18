ab8be0d384ba6f6de254597585d179f635b1c14c
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-18 09:57:42 -0400

    initial commit

---
+++ src/main/java/com/example/demo/BooksAuthorOne2ManyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAuthorOne2ManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksAuthorOne2ManyApplication.class, args);
+    }
+
+}
